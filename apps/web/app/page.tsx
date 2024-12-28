74f90ee08298a21f9e30baa7abbf59e45995ae57
derodero24 <shintaro@payn.io>
2024-12-28 19:42:45 +0900

    Calendar追加

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,24 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
+
+import { Button } from "@workspace/ui/components/button";
+import { Calendar } from "@workspace/ui/components/calendar";
+import React from "react";
 
 export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
-    </div>
-  )
+	const [date, setDate] = React.useState<Date | undefined>(new Date());
+
+	return (
+		<div className="flex items-center justify-center min-h-svh">
+			<div className="flex flex-col items-center justify-center gap-4">
+				<h1 className="text-2xl font-bold">Hello World</h1>
+				<Button size="sm">Button</Button>
+				<Calendar
+					mode="single"
+					selected={date}
+					onSelect={setDate}
+					className="rounded-md border"
+				/>
+			</div>
+		</div>
+	);
 }
