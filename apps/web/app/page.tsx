82e3bbbb030c4217be4ffdc19bad8e4864883dc5
derodero24 <shintaro@payn.io>
2024-12-28 20:39:49 +0900

    lint, formatの自動修正

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,24 +1,24 @@
-"use client";
+'use client';
 
-import { Button } from "@workspace/ui/components/button";
-import { Calendar } from "@workspace/ui/components/calendar";
-import React from "react";
+import { Button } from '@workspace/ui/components/button';
+import { Calendar } from '@workspace/ui/components/calendar';
+import React from 'react';
 
 export default function Page() {
-	const [date, setDate] = React.useState<Date | undefined>(new Date());
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
 
-	return (
-		<div className="flex items-center justify-center min-h-svh">
-			<div className="flex flex-col items-center justify-center gap-4">
-				<h1 className="text-2xl font-bold">Hello World</h1>
-				<Button size="sm">Button</Button>
-				<Calendar
-					mode="single"
-					selected={date}
-					onSelect={setDate}
-					className="rounded-md border"
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className='flex items-center justify-center min-h-svh'>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <h1 className='text-2xl font-bold'>Hello World</h1>
+        <Button size='sm'>Button</Button>
+        <Calendar
+          mode='single'
+          selected={date}
+          onSelect={setDate}
+          className='rounded-md border'
+        />
+      </div>
+    </div>
+  );
 }
